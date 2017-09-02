1b162d7175ce4791f17a6e0a921861ceaaf17b84
Luke Pulverenti <luke.pulverenti@gmail.com>
2017-09-02 14:05:52 -0400

    initial

---
+++ MediaBrowser.Plugins.NextPvr/Plugin.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Plugins;
+using MediaBrowser.Model.Serialization;
+using MediaBrowser.Plugins.NextPvr.Configuration;
+
+namespace MediaBrowser.Plugins.NextPvr
+{
+    /// <summary>
+    /// Class Plugin
+    /// </summary>
+    public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
+    {
+        public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+            : base(applicationPaths, xmlSerializer)
+        {
+            Instance = this;
+        }
+
+        public IEnumerable<PluginPageInfo> GetPages()
+        {
+            return new[]
+            {
+                new PluginPageInfo
+                {
+                    Name = Name,
+                    EmbeddedResourcePath = GetType().Namespace + ".Configuration.configPage.html"
+                }
+            };
+        }
+
+        private Guid _id = new Guid("9574ac10-bf23-49bc-949f-924f23cfa48f");
+        public override Guid Id
+        {
+            get { return _id; }
+        }
+
+        /// <summary>
+        /// Gets the name of the plugin
+        /// </summary>
+        /// <value>The name.</value>
+        public override string Name
+        {
+            get { return "Next Pvr"; }
+        }
+
+        /// <summary>
+        /// Gets the description.
+        /// </summary>
+        /// <value>The description.</value>
+        public override string Description
+        {
+            get
+            {
+                return "Provides live tv using next pvr as a back-end.";
+            }
+        }
+
+        /// <summary>
+        /// Gets the instance.
+        /// </summary>
+        /// <value>The instance.</value>
+        public static Plugin Instance { get; private set; }
+    }
+}
