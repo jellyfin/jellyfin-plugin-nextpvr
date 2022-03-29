0bfa8aefc445ba9241b85b97a9e799895ec1f6d6
emveepee <nospam>
2022-03-29 15:49:50 -0400

    Remove test code
    Code used for testing made it into production

--- Jellyfin.Plugin.NextPVR/Responses/ListingsResponse.cs
+++ Jellyfin.Plugin.NextPVR/Responses/ListingsResponse.cs
@@ -66,7 +66,6 @@ public class ListingsResponse
         if (epg.Genres != null)
         {
             info.Genres = epg.Genres;
-            info.Genres[0] += " Movie";
             genreMapper.PopulateProgramGenres(info);
             if (info.IsMovie)
             {
